14db4b0ed8c1098d5c8f22a951c710ea461980ca
Paul Mairot <paul.mairot@heig-vd.ch>
2022-04-13 08:24:23 +0200

    Exercice 4 - Partie 3

--- 04-gapminder/src/index.js
+++ 04-gapminder/src/index.js
@@ -4,6 +4,7 @@ import gdp from '../data/income_per_person_gdppercapita_ppp_inflation_adjusted.c
 import expectancy from '../data/life_expectancy_years.csv';
 import population from '../data/population_total.csv';
 
+
 // Pour importer les données
 // import file from '../data/data.csv'
 
@@ -21,6 +22,13 @@ function getValue (val) {
         }
     }
 }
+
+// Convert value to number
+gdp.forEach(row => {
+  if (typeof row["2021"] === 'string') {
+      row["2021"] = getValue(row["2021"]);
+  } 
+});
 
 //EXERCICE 1
 /*
@@ -91,6 +99,7 @@ svg.selectAll("circle").data(population).join()
 */
 
 /* EXERCICE 2 */
+/*
 let listCountries = []
 
 expectancy.forEach(row => {
@@ -146,4 +155,153 @@ let colorScale = d3.scaleThreshold()
         return colorScale(number);
       })
   })
+ */   
+
+
+/* EXERCICE 3 */
+
+let startYear = Object.keys(gdp[0])[0];
+let endYear = Object.keys(gdp[0])[Object.keys(gdp[0]).length-2];
+
+
+// Convert value to number
+gdp.forEach(row => {
+  for (let i = startYear; i <= endYear; i++) {
+    if (typeof row[(i).toString()] === 'string') {
+      row[(i).toString()] = getValue(row[(i).toString()]);
+    } 
+  } 
+});
+
+population.forEach(row => {
+  for (let i = startYear; i <= 2100; i++) {
+    if (typeof row[(i).toString()] === 'string') {
+      row[(i).toString()] = getValue(row[(i).toString()]);
+    } 
+  }
+});
+
+
+let margin = {top: 20, right: 20, bottom: 30, left: 50},
+width = 650 - margin.left - margin.right,
+height = 500 - margin.top - margin.bottom;
+
+let svg = d3.select("#graph")
+.append("svg")
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+let x = d3.scaleLinear()
+.domain([0, 10000])
+.range([ 0, width ]);
+svg.append("g")
+.attr("transform", "translate(0," + height + ")")
+.call(d3.axisBottom(x));
+
+// Add Y axis
+let y = d3.scalePow()
+.exponent(1.5)
+.domain([0, 100])
+.range([ height, 0]);
+svg.append("g")
+.call(d3.axisLeft(y));
+
+var z = d3.scaleLinear()
+.domain([200000, 1310000000])
+.range([ 4, 60]);
+
+
+let nIntervId;
+
+function animate() {
+  if (!nIntervId) {
+		nIntervId = setInterval(play, 10);
     
+	}
+}
+
+let i = startYear;
+
+function play() {
+		if(i == endYear) {
+		  stop
+		} else {
+		  i++;
+		}
+ 
+    // Update graph
+		d3.select('#year').text(i)
+    
+    svg.append('g')
+    .selectAll("dot")
+    .data(gdp)
+    .enter()
+    .append("circle")
+    .attr("cx", function (d) { return x(d[(i)]); } )
+    .attr("r", 10 )
+    .style("fill", "#6933af")
+    .style("opacity", "0.7")
+    .attr("stroke", "black")
+
+    svg.selectAll("circle").data(expectancy).join()
+    .attr("cy", function (d) { return y(d[(i).toString()]); } )
+
+    svg.selectAll("circle").data(population).join()
+    .attr("r", function (d) { return z(d[(i).toString()]); } )
+}
+
+function stop() {
+  clearInterval(nIntervId);
+  nIntervId = null;
+}
+
+document.getElementById("play").addEventListener("click", animate);
+document.getElementById("stop").addEventListener("click", stop);
+
+/*
+gdp.forEach(row => {
+  console.log(row[2021]);
+});
+*/
+
+let data = [];
+
+for (let i = 1800; i < 2100; i++) {
+  data.push(i)
+
+  for (let j = 0; j < population.length; j++) {
+    let entry = {
+      country : population[j]["country"],
+      pop : population[j][i]
+    }
+    data.push(entry);
+  }
+  
+  //data.push(countries);
+  //data.push(pop);
+}
+
+//console.log(data);
+
+/*
+function mergeArrayObjects(arr1,arr2){
+  let start = 0;
+  let merge = [];
+
+  while(start < arr1.length){
+    if(arr1[start]["country"] === arr2[start]["country"]){
+         //pushing the merged objects into array
+        merge.push({...arr1[start],...arr2[start]})
+    }
+    //incrementing start value
+    start = start+1
+  }
+  return merge;
+}
+
+let merged = mergeArrayObjects(gdp, population)
+console.log(merged);
+console.log(population[0]["country"]);
+*/
