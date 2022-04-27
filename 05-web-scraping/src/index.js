b6f51efc358c6631de263702026fc46286f7033c
Paul Mairot <paul.mairot@heig-vd.ch>
2022-04-27 16:12:51 +0200

    Exercice 5 - Partie 3

--- 05-web-scraping/src/index.js
+++ 05-web-scraping/src/index.js
@@ -2,7 +2,7 @@ import jsdom from "jsdom";
 import fetch from "isomorphic-fetch"
 import puppeteer from "puppeteer"
 
-// Partie 1 - Screenshot Wikipedia
+/* Partie 1 - Screenshot Wikipedia */
 /*
 (async () => {
     const browser = await puppeteer.launch({
@@ -17,7 +17,8 @@ import puppeteer from "puppeteer"
 */
 
 
-// Partie 2 - Webscraper Wikipedia
+/* Partie 2 - Webscraper Wikipedia */
+/*
 (async () => {
     const url = 'https://fr.wikipedia.org/wiki/Canton_(Suisse)';
     const browser = await puppeteer.launch();
@@ -32,9 +33,6 @@ import puppeteer from "puppeteer"
           return Array.from(columns, column => column.innerText);
         });
       });
-
-      // de 2 à 27
-      //console.log(rawData[2][0]);
 
       // Prepare array with values
       let result = [];
@@ -62,3 +60,39 @@ import puppeteer from "puppeteer"
       console.log('error', error);
     }
   })();
+*/
+
+/* Partie 3 - Webscraper e-commerce */
+(async () => {
+    const url = 'https://www.webscraper.io/test-sites/e-commerce/allinone/computers/laptops';
+    const browser = await puppeteer.launch();
+  
+    try {
+        const page = await browser.newPage();
+        await page.goto(url);
+
+        let productList = [];
+
+        let div = await page.$$('div.thumbnail')
+
+        for (let el of div) {
+            let product = await el.$eval('.title', el => el.textContent);
+            let price = await el.$eval('.price', el => el.textContent);
+            let stars = await el.$eval('.ratings :nth-child(2)', el => el.getAttribute( 'data-rating' ));
+            stars = parseInt(stars);
+            
+            let productComplete = {
+                produit: product,
+                prix: price,
+                etoiles: stars
+            }
+
+            productList.push(productComplete);
+        }
+
+        console.table(productList);
+        
+    } catch (error) {
+      console.log('error', error);
+    }
+})();
